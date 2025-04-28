c0cba9daf25757c0addcf067d4f2fcbb5fa131c8
amethyst2093 <wonton0409@gmail.com>
2025-04-28 09:15:39 -0400

    movable elephant + falling apple

---
+++ Elephant.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The Elephant, our hero
+ * 
+ * @author Amelie 
+ * @version Apr 2025
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("left")){
+            move(-1);
+        }
+        if(Greenfoot.isKeyDown("right")){
+            move(1);
+        }
+    }
+}
