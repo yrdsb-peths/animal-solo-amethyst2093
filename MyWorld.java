af40cd0f8ce94fa35bc9c97f3551b7d26715265f
amethyst2093 <wonton0409@gmail.com>
2025-04-29 08:42:20 -0400

    add scoreLabel

--- MyWorld.java
+++ MyWorld.java
@@ -3,9 +3,12 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        //create elephant
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
-        
+        //create label
+        Label scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
         createApple();
     }
     /**
