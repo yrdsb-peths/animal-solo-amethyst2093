c56a07e3e35870e95c14f34e8339f50504fcda16
amethyst2093 <wonton0409@gmail.com>
2025-04-30 08:45:20 -0400

    animate elephant

--- Elephant.java
+++ Elephant.java
@@ -9,27 +9,37 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephant sound.mp3");
-    GreenfootImage idle = new GreenfootImage("images/elephant_idle/idle0.png");
+    GreenfootImage[] idle = new GreenfootImage[8];
     /**
      * constructor - code gets run first time instance created
      */
     public Elephant(){
-        setImage(idle);
+        for(int i = 0; i<idle.length; i++){
+            idle[i] = new GreenfootImage("images/elephant_idle/idle"+i+".png");
+        }
+        setImage(idle[0]);
     }
-    
+    int imageIndex = 0;
+    /**
+     * animate elephant
+     */
+    public void animateElephant(){
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
     /**
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        if(Greenfoot.isKeyDown("left")){
-            move(-5);
+        if(Greenfoot.isKeyDown("left")){            move(-5);
         }
         if(Greenfoot.isKeyDown("right")){
             move(5);
         }
         eat();
+        animateElephant();
     }
     //remove apples if eaten
     /**
