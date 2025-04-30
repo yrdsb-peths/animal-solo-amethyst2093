6b96c415f79d8f5dffc193cad6dac609a7a1933d
amethyst2093 <wonton0409@gmail.com>
2025-04-30 09:24:15 -0400

    replaced apple

--- Apple.java
+++ Apple.java
@@ -8,6 +8,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Apple extends Actor
 {
+    public Apple(){
+        GreenfootImage apple = new GreenfootImage("apple.png");
+        apple.scale(50, 50);
+        setImage(apple);
+    }
     /**
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
