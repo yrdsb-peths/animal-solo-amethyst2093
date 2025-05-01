be3dbe16aec9934712cfb2b3326878ecf6f527d0
amethyst2093 <wonton0409@gmail.com>
2025-05-01 09:24:08 -0400

    unicode arrows + center elements

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -17,7 +16,7 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-
+        Label titleLabel = new Label("Hungry Elephant", 60);
         addObject(titleLabel, getWidth()/2, 100);
         prepare();
     }
@@ -37,18 +36,8 @@ public class TitleScreen extends World
     private void prepare()
     {
         Elephant elephant = new Elephant();
-        addObject(elephant,289,161);
-        elephant.setLocation(284,208);
-        elephant.setLocation(228,236);
-        Label label = new Label("use arrow keys to move left and right", 20);
-        addObject(label,275,321);
-        label.setLocation(305,322);
-        label.setLocation(286,322);
-        elephant.setLocation(308,198);
-        elephant.setLocation(300,199);
-        elephant.setLocation(289,199);
-        label.setLocation(305,305);
-        label.setLocation(365,297);
-        label.setLocation(294,312);
+        addObject(elephant,300,200);
+        Label label = new Label("use \u2190 and \u2192 to move left and right", 20);
+        addObject(label,300,300);
     }
 }
